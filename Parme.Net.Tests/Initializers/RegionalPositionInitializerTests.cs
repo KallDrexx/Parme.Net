56ae6fcbbe24824b0cf089fe083f7b4f1c2a92e3
KallDrexx <me@mshapiro.net>
2021-04-24 15:03:58 -0400

    Added regional position initializer

---
+++ Parme.Net.Tests/Initializers/RegionalPositionInitializerTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using Parme.Net.Initializers;
+using Shouldly;
+using Xunit;
+
+namespace Parme.Net.Tests.Initializers
+{
+    public class RegionalPositionInitializerTests : TestBase
+    {
+        [Fact]
+        public void Can_Be_Cloned()
+        {
+            var initializer = new RegionalPositionInitializer(new Random())
+            {
+                MinRelativePosition = new Vector2(3, 4),
+                MaxRelativePosition = new Vector2(5, 6),
+            };
+
+            var cloned = (RegionalPositionInitializer) initializer.Clone();
+            cloned.MinRelativePosition.ShouldBe(new Vector2(3, 4));
+            cloned.MaxRelativePosition.ShouldBe(new Vector2(5, 6));
+        }
+        
+        [Fact]
+        public void Sets_Initialized_Position_For_New_Particles_Based_On_Emitter_Coordinates()
+        {
+            var initializer = new RegionalPositionInitializer(new Random())
+            {
+                MinRelativePosition = new Vector2(3, 4),
+                MaxRelativePosition = new Vector2(3, 4),
+            };
+            
+            var config = new EmitterConfig
+            {
+                Initializers = {initializer},
+                Trigger = MockTrigger().Object,
+                InitialCapacity = 10,
+            };
+
+            var allocator = new ParticleAllocator(100);
+            var emitter = new ParticleEmitter(allocator, config)
+            {
+                WorldCoordinates = new Vector2(10, 11),
+            };
+            
+            var collection = new ParticleCollection(emitter.Reservation)
+            {
+                ValidPropertiesToSet = initializer.PropertiesISet,
+            };
+            
+            var newIndices = new[] {1, 3, 5, 7};
+            initializer.InitializeParticles(emitter, collection, newIndices);
+
+            var positionX = collection.GetPropertyValues<float>(StandardParmeProperties.PositionX.Name);
+            var positionY = collection.GetPropertyValues<float>(StandardParmeProperties.PositionY.Name);
+            foreach (var index in newIndices)
+            {
+                positionX[index].ShouldBe(13);
+                positionY[index].ShouldBe(15);
+            }
+        }
+
+        [Fact]
+        public void Non_New_Particles_Do_Not_Have_Their_Position_Set()
+        {
+            var initializer = new RegionalPositionInitializer(new Random())
+            {
+                MinRelativePosition = new Vector2(3, 4),
+                MaxRelativePosition = new Vector2(3, 4),
+            };
+            
+            var config = new EmitterConfig
+            {
+                Initializers = {initializer},
+                Trigger = MockTrigger().Object,
+                InitialCapacity = 10,
+            };
+
+            var allocator = new ParticleAllocator(100);
+            var emitter = new ParticleEmitter(allocator, config);
+            var collection = new ParticleCollection(emitter.Reservation)
+            {
+                ValidPropertiesToSet = initializer.PropertiesISet,
+            };
+
+            {
+                var positionX = collection.GetPropertyValues<float>(StandardParmeProperties.PositionX.Name);
+                var positionY = collection.GetPropertyValues<float>(StandardParmeProperties.PositionY.Name);
+                for (var index = 0; index < positionX.Length; index++)
+                {
+                    positionX[index] = 9.9f;
+                    positionY[index] = 8.8f;
+                }
+            }
+            
+            var newIndices = new[] {1, 3, 5, 7};
+            initializer.InitializeParticles(emitter, collection, newIndices);
+
+            {
+                var positionX = collection.GetPropertyValues<float>(StandardParmeProperties.PositionX.Name);
+                var positionY = collection.GetPropertyValues<float>(StandardParmeProperties.PositionY.Name);
+                foreach (var index in Enumerable.Range(0, positionX.Length).Except(newIndices))
+                {
+                    positionX[index].ShouldBe(9.9f);
+                    positionY[index].ShouldBe(8.8f);
+                }
+            }
+        }
+    }
+}
---
+++ Parme.Net/Initializers/RegionalPositionInitializer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Parme.Net.Initializers
+{
+    /// <summary>
+    /// An initializer which positions new particles in a random area relative to the emitter
+    /// </summary>
+    public class RegionalPositionInitializer : IParticleInitializer
+    {
+        private readonly Random _random;
+        
+        /// <summary>
+        /// The smallest X and Y coordinates the particle can be spawned at, relative to the emitter.  It is expected
+        /// that the maximum X and Y values specified are both larger than the minimum X and Y values.
+        /// </summary>
+        public Vector2 MinRelativePosition { get; set; }
+        
+        /// <summary>
+        /// The largest X and Y coordinates the particle can be spawned at, relative to the emitter.  It is expected
+        /// that the maximum X and Y values specified are both larger than the minimum X and Y values.
+        /// </summary>
+        public Vector2 MaxRelativePosition { get; set; }
+        
+        public HashSet<ParticleProperty> PropertiesISet { get; } = new(new[]
+        {
+            StandardParmeProperties.PositionX,
+            StandardParmeProperties.PositionY,
+        });
+
+        public RegionalPositionInitializer(Random random)
+        {
+            _random = random;
+        }
+        
+        public IParticleInitializer Clone()
+        {
+            return new RegionalPositionInitializer(_random)
+            {
+                MinRelativePosition = MinRelativePosition,
+                MaxRelativePosition = MaxRelativePosition,
+            };
+        }
+
+        public void InitializeParticles(ParticleEmitter emitter, ParticleCollection particles, IReadOnlyList<int> newParticleIndices)
+        {
+            var positionX = particles.GetPropertyValues<float>(StandardParmeProperties.PositionX.Name);
+            var positionY = particles.GetPropertyValues<float>(StandardParmeProperties.PositionY.Name);
+            foreach (var index in newParticleIndices)
+            {
+                var x = MinRelativePosition.X + _random.NextDouble() * (MaxRelativePosition.X - MinRelativePosition.X);
+                var y = MinRelativePosition.Y + _random.NextDouble() * (MaxRelativePosition.Y - MinRelativePosition.Y);
+
+                positionX[index] = emitter.WorldCoordinates.X + (float) x;
+                positionY[index] = emitter.WorldCoordinates.Y + (float) y;
+            }
+        }
+    }
+}
